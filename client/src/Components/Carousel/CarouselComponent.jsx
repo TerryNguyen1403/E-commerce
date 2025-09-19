bcdf4c24d11ff85042a92296489886408d313a1c
TerryNguyen1403 <giangnht1403@gmail.com>
2025-09-19 13:04:37 +0700

    Add Carousel into Home pages

---
+++ client/src/Components/Carousel/CarouselComponent.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect} from 'react'
+import { Carousel, Container, Row, Col } from "react-bootstrap";
+import Spinner from 'react-bootstrap/Spinner';
+import axios from 'axios'
+import './CarouselComponent.css'
+import errorImg from '../../assets/404.jpg'
+
+const CarouselComponent = () => {
+    const [featuredProducts, setFeaturedProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchFeaturedProducts = async () => {
+        try {
+            const res = await axios.get("http://localhost:4000/api/product/featured-products");
+            setFeaturedProducts(res.data.featuredProducts);
+
+        } catch (error) {
+            console.error("Lỗi khi fetch sản phẩm: ", error);
+        } finally {
+            setLoading(false);
+        }
+        }
+
+        fetchFeaturedProducts();
+    }, []);
+
+    return (
+        <Container fluid className="mt-3">
+            <Row className="justify-content-center">
+                {/* Cột giữa - Carousel chính */}
+                <Col md={8} lg={9} xl={10}>
+                    <div className="main-carousel-container position-relative">
+                        {loading ? (
+                            <div className="d-flex justify-content-center align-items-center" style={{ height: "300px" }}>
+                                <Spinner animation="border" role="status">
+                                    <span className="visually-hidden">Đang tải...</span>
+                                </Spinner>
+                            </div>
+                        ) : featuredProducts.length > 0 ? (
+                        <Carousel
+                            fade
+                            interval={3000}
+                            controls={true}
+                            indicators={true}
+                            className="main-carousel"
+                        >
+                            {featuredProducts.slice(0, 5).map((product) => (
+                            <Carousel.Item
+                                key={product._id}
+                                className="carousel-item-custom"
+                            >
+                                <div className="carousel-image-container">
+                                    <img
+                                        className="d-block w-100 carousel-image"
+                                        src={product.image}
+                                        alt={product.name}
+                                    />
+
+                                    {/* Overlay gradient */}
+                                    <div className="carousel-overlay"></div>
+                                </div>
+                            </Carousel.Item>
+                            ))}
+                        </Carousel>
+                        ) : (
+                        <Carousel className="main-carousel">
+                            <Carousel.Item>
+                                <div className="carousel-image-container">
+                                    <img
+                                    className="d-block w-100 carousel-image"
+                                    src={errorImg}
+                                    alt=""
+                                    />
+                                </div>
+                            </Carousel.Item>
+                        </Carousel>
+                        )}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default CarouselComponent
---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+// Import components
+import CarouselComponent from "../Components/Carousel/CarouselComponent";
+// import NewProducts from "../components/NewProducts";
+
+const Home = () => {
+  return (
+    <div>
+      <CarouselComponent />
+      {/* <NewProducts /> */}
+    </div>
+  );
+};
+
+export default Home;
