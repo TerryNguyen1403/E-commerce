27730a766b2cfd6b576954f4ed2570bff32106df
TerryNguyen1403 <giangnht1403@gmail.com>
2025-09-24 08:26:00 +0700

    Adding new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// Import components
+import NavbarWrapper from './Components/NavbarComponent/NavbarWrapper'
+import Footer from './Components/Footer'
+
+// Import pages
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Home from './Pages/Home'
+import Product from './Pages/Product'
+import DisplayProduct from './Pages/DisplayProduct'
+import ScrollToTop from './Components/ScrollToTop'
+
 function App() {
   return (
     <BrowserRouter>
+      <NavbarWrapper />
       <Routes>
-        
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+
+        {/* Product routes */}
+        <Route path="/product" element={<Product />}>
+          <Route path=":productId" element={<Product />}/>
+        </Route>
+        <Route path='/new-products' element={<DisplayProduct endpoint='new-products'/>}/>
+        <Route path='/hot-products' element={<DisplayProduct endpoint='hot-products'/>}/>
+        <Route path='/windows' element={<DisplayProduct endpoint='windows'/>}/>
+        <Route path='/playstation' element={<DisplayProduct endpoint='playstation'/>}/>
+        <Route path='/nintendo' element={<DisplayProduct endpoint='nintendo'/>}/>
       </Routes>
+      <ScrollToTop />
+      <Footer />
     </BrowserRouter>
   )
 }
