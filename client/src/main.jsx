e4a937ece99d7a86b02b9e6a0fbf2ddc68c1eebc
TerryNguyen1403 <giangnht1403@gmail.com>
2025-10-08 07:39:12 +0700

    Add CartContextProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ProductContextProvider from './Context/ProductContext.jsx'
+import CartContextProvider from './Context/CartContext.jsx'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
 createRoot(document.getElementById('root')).render(
   <ProductContextProvider>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
   </ProductContextProvider>,
 )
